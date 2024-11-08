f7318f75e09bd4f7fe799dc11ac4c00ddc761e54
Antoine Bastos <antoine.bastos@lunatech.fr>
2024-11-08 16:56:52 +0100

    GenericFunctionExercises

--- exercises/src/main/scala/exercises/generic/GenericFunctionExercises.scala
+++ exercises/src/main/scala/exercises/generic/GenericFunctionExercises.scala
@@ -17,20 +17,18 @@ object GenericFunctionExercises {
   case class Pair[A](first: A, second: A) {
     // 1a. Implement `swap` which exchanges `first` and `second`
     // such as Pair("John", "Doe").swap == Pair("Doe", "John")
-    def swap: Pair[A] =
-      ???
+    def swap: Pair[A] = Pair(second, first)
 
     // 1b. Implement `map` which applies a function to `first` and `second`
     // such as Pair("John", "Doe").map(_.length) == Pair(4,3)
-    def map[To](update: A => To): Pair[To] =
-      ???
+    def map[To](update: A => To): Pair[To] = Pair(update(first), update(second))
 
     // 1c. Implement `zipWith` which merges two Pairs using a `combine` function
     // such as Pair(0, 2).zipWith(Pair(3, 4))((x, y) => x + y) == Pair(3, 6)
     //         Pair(2, 3).zipWith(Pair("Hello ", "World "))(replicate) == Pair("Hello Hello ", "World World World ")
     // Bonus: Why did we separate the arguments of `zipWith` into two set of parentheses?
-    def zipWith[Other, To](other: Pair[Other])(combine: (A, Other) => To): Pair[To] =
-      ???
+    def zipWith[Other, To](other: Pair[Other])(combine: (A, Other) => To):
+      Pair[To] = Pair(combine(first, other.first), combine(second, other.second))
   }
 
   // 1d. Use the Pair API to decode the content of `secret`.
@@ -42,7 +40,10 @@ object GenericFunctionExercises {
       first = List(103, 110, 105, 109, 109, 97, 114, 103, 111, 114, 80),
       second = List(108, 97, 110, 111, 105, 116, 99, 110, 117, 70)
     )
-  lazy val decoded: Pair[String] = ???
+  val decoded: Pair[String] = secret
+    .map(bytes => new String(bytes.toArray))
+    .map(_.reverse)
+    .swap
 
   // 1e. Use the Pair API to combine `productNames` and `productPrices` into `products`
   // such as products == Pair(Product("Coffee", 2.5), Product("Plane ticket", 329.99))
@@ -51,8 +52,7 @@ object GenericFunctionExercises {
   val productNames: Pair[String]  = Pair("Coffee", "Plane ticket")
   val productPrices: Pair[Double] = Pair(2.5, 329.99)
 
-  lazy val products: Pair[Product] =
-    ???
+  lazy val products: Pair[Product] = productNames.zipWith(productPrices)(Product)
 
   //////////////////////////////////////////////
   // Bonus question (not covered by the video)
@@ -61,6 +61,9 @@ object GenericFunctionExercises {
   // 1f. Can you implement a method on `Pair` similar to `zipWith`, but that combines 3
   // Pairs instead of 2? If yes, can you implement this method using `zipWith`?
   // Note: Libraries often call this method `map3` and `zipWith` is often called `map2`
+
+  def map3[A, B, C, D, To](p: Pair[A])(p1: Pair[B])(f1: (A, B) => C)(p2: Pair[D])(f2: (C, D) => To): Pair[To] =
+    p.zipWith(p1)(f1).zipWith(p2)(f2)
 
   ////////////////////////////
   // Exercise 2: Predicate
@@ -86,7 +89,7 @@ object GenericFunctionExercises {
     //         (isEven && isPositive)(-4) == false
     //         (isEven && isPositive)(-7) == false
     def &&(other: Predicate[A]): Predicate[A] =
-      ???
+      Predicate(v => eval(v) && other(v))
 
     // 2b. Implement `||` that combines two predicates using logical or
     // such as (isEven || isPositive)(12) == true
@@ -94,12 +97,20 @@ object GenericFunctionExercises {
     //         (isEven || isPositive)(-4) == true
     // but     (isEven || isPositive)(-7) == false
     def ||(other: Predicate[A]): Predicate[A] =
-      ???
+      Predicate(v => eval(v) || other(v))
 
     // 2c. Implement `flip` that reverses a predicate
     // such as isEven.flip(11) == true
     def flip: Predicate[A] =
-      ???
+      Predicate(c => !eval(c))
+
+    def contramap[From](zoom: From => A): Predicate[From] = Predicate(u => eval(zoom(u)))
+
+  }
+
+  object Predicate {
+    def False[A]: Predicate[A] = Predicate(_ => false)
+    def True[A]: Predicate[A] = False.flip
   }
 
   // 2d. Implement `isValidUser`, a predicate which checks if a `User` is:
@@ -114,8 +125,21 @@ object GenericFunctionExercises {
   // You may want to create new Predicate methods to improve the implementation of `isValidUser`.
   case class User(name: String, age: Int)
 
-  lazy val isValidUser: Predicate[User] =
-    ???
+  lazy val isValidUser: Predicate[User] = {
+    //isAdult && isUsernameLongerThan3 && isUsernameCapitalised
+    byUser(_.age)(isBiggerThan(18)) &&
+      byUser(_.name)(isLongerThan(3)) && isUsernameCapitalised
+  }
+
+  def byUser[To](zoom: User => To)(predicate: Predicate[To]): Predicate[User] =
+    Predicate(u => predicate(zoom(u)))
+
+  private val isAdult: Predicate[User] = isBiggerThan(3).contramap(_.age)
+  private val isUsernameLongerThan3: Predicate[User] = isBiggerThan(3).contramap(_.name.length)
+  private val isUsernameCapitalised: Predicate[User] = Predicate(_.name.charAt(0).isUpper)
+
+  def isBiggerThan(min: Int): Predicate[Int] = Predicate(_>=min)
+  def isLongerThan(min: Int): Predicate[String] = isBiggerThan(min).contramap(_.length)
 
   ////////////////////////////
   // Exercise 3: JsonDecoder
@@ -124,9 +148,24 @@ object GenericFunctionExercises {
   // very basic representation of JSON
   type Json = String
 
-  trait JsonDecoder[A] {
+  trait JsonDecoder[A] { outer =>
     def decode(json: Json): A
-  }
+
+    def map[To](update: A => To): JsonDecoder[To] = {
+      (json: Json) => update(decode(json))
+    }
+
+    def orElse(other: JsonDecoder[A]): JsonDecoder[A] = {
+        new JsonDecoder[A] {
+          def decode(json: Json): A = {
+            Try(outer.decode(json)) match {
+              case Success(value) => value
+              case Failure(_) => other.decode(json)
+            }
+          }
+        }
+    }
+   }
 
   val intDecoder: JsonDecoder[Int] = new JsonDecoder[Int] {
     def decode(json: Json): Int = json.toInt
@@ -144,12 +183,13 @@ object GenericFunctionExercises {
   val intDecoderSAM: JsonDecoder[Int] =
     (json: Json) => json.toInt
 
+  val longDecoderSAM: JsonDecoder[Long] = (json: Json) => json.toLong
+
   // 3a. Implement `userIdDecoder`, a `JsonDecoder` for the `UserId` case class
   // such as userIdDecoder.decode("1234") == UserId(1234)
   // but     userIdDecoder.decode("hello") would throw an Exception
   case class UserId(value: Int)
-  lazy val userIdDecoder: JsonDecoder[UserId] =
-    ???
+  lazy val userIdDecoder: JsonDecoder[UserId] = intDecoder.map(UserId)
 
   // 3b. Implement `localDateDecoder`, a `JsonDecoder` for `LocalDate`
   // such as localDateDecoder.decode("\"2020-03-26\"") == LocalDate.of(2020,3,26)
@@ -157,16 +197,17 @@ object GenericFunctionExercises {
   // and     localDateDecoder.decode("hello") would throw an Exception
   // Note: You can parse a `LocalDate` using `LocalDate.parse` with a java.time.format.DateTimeFormatter
   // e.g. DateTimeFormatter.ISO_LOCAL_DATE
-  lazy val localDateDecoder: JsonDecoder[LocalDate] =
-    ???
+  lazy val localDateDecoder: JsonDecoder[LocalDate] = {
+    stringDecoder.map(LocalDate.parse(_, DateTimeFormatter.ISO_LOCAL_DATE))
+  }
 
   // 3c. Implement `map` a generic function that converts a `JsonDecoder` of `From`
   // into a `JsonDecoder` of `To`.
   // Bonus: Can you re-implement `userIdDecoder` and `localDateDecoder` using `map`
   def map[From, To](decoder: JsonDecoder[From])(update: From => To): JsonDecoder[To] =
-    ???
+    (json: Json) => update(decoder.decode(json))
 
-  // 3d. Move `map` inside of `JsonDecoder` trait so that we can use the syntax
+  // 3d. Move `map` inside `JsonDecoder` trait so that we can use the syntax
   // `intDecoder.map(_ + 1)` instead of `map(intDecoder)(_ + 1)`
 
   // 3e. Imagine we have to integrate with a weird JSON API where dates are sometimes encoded
@@ -177,8 +218,10 @@ object GenericFunctionExercises {
   // but weirdLocalDateDecoder.decode("hello") would throw an Exception
   // Try to think how we could extend JsonDecoder so that we can easily implement
   // other decoders that follow the same pattern.
-  lazy val weirdLocalDateDecoder: JsonDecoder[LocalDate] =
-    ???
+  val longJsonDecoder: JsonDecoder[Long] = _.toLong
+  val longLocalDateDecoder: JsonDecoder[LocalDate] = longJsonDecoder.map(LocalDate.ofEpochDay)
+
+  lazy val weirdLocalDateDecoder: JsonDecoder[LocalDate] = localDateDecoder orElse longLocalDateDecoder
 
   //////////////////////////////////////////////
   // Bonus question (not covered by the video)
@@ -191,8 +234,10 @@ object GenericFunctionExercises {
   // * "\"null\"" into a Some("null")
   // * "null" into "None"
   // Note: you may need to change the function signature
-  def optionDecoder[A]: JsonDecoder[Option[A]] =
-    ???
+  def optionDecoder[A](decoder: JsonDecoder[A]): JsonDecoder[Option[A]] =
+    (json: Json) => {
+      Try(decoder.decode(json)).toOption
+    }
 
   // 3g. `JsonDecoder` currently throws an exception if the input is not a valid JSON.
   // How could you change the API so that it doesn't happen anymore?
--- exercises/src/test/scala/exercises/generic/GenericFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/generic/GenericFunctionExercisesTest.scala
@@ -16,13 +16,19 @@ class GenericFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProp
   // Exercise 1: Pair
   ////////////////////
 
-  test("Pair swap") {}
+  test("Pair swap") {
+    assert(Pair(0, 1).swap == Pair(1, 0))
+  }
 
-  test("Pair map") {}
+  test("Pair map") {
+    assert(Pair(0, 1).map(identity) == Pair(0, 1))
+  }
 
   test("Pair decoded") {}
 
-  test("Pair zipWith") {}
+  test("Pair zipWith") {
+    assert(Pair(0, 1).zipWith(Pair(1,2))((a,b) => a+b) == Pair(1,3))
+  }
 
   test("Pair productNames") {}
 
@@ -30,20 +36,100 @@ class GenericFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProp
   // Exercise 2: Predicate
   ////////////////////////////
 
-  test("Predicate &&") {}
+  def False[A]: Predicate[A] = Predicate(_ => false)
+  def True[A]: Predicate[A] = Predicate(_ => true)
 
-  test("Predicate ||") {}
+  test("Predicate &&") {
+    assert((isEven && isPositive)(12) == true)
+    assert((isEven && isPositive)(11) == false)
+    assert((isEven && isPositive)(-4) == false)
+    assert((isEven && isPositive)(-7) == false)
+  }
 
-  test("Predicate flip") {}
+  test("Predicate && BPT") {
+    forAll{ (evalA: (Int => Boolean), evalB: (Int => Boolean), value: Int) =>
+      val p1 = Predicate(evalA)
+      val p2 = Predicate(evalB)
+
+      assert((p1 && False)(value) == false)
+      assert((p1 && True)(value) == p1(value))
+    }
+  }
+
+  test("Predicate ||") {
+    forAll{ (evalA: (Int => Boolean), evalB: (Int => Boolean), value: Int) =>
+      val p1 = Predicate(evalA)
+      val p2 = Predicate(evalB)
+
+      assert((p1 || False)(value) == p1(value))
+      assert((p1 || True)(value) == true)
+    }
+  }
+
+  test("Predicate flip") {
+    assert(Predicate.True.flip(()) == false)
+    assert(Predicate.False.flip(()) == true)
+  }
+
+  test("isValidUser") {
+    assert(isValidUser(User("John", 20)) == true)
+    assert(isValidUser(User("John", 17)) == false)
+    assert(isValidUser(User("john", 20)) == false)
+    assert(isValidUser(User("x"   , 23)) == false)
+  }
 
   ////////////////////////////
   // Exercise 3: JsonDecoder
   ////////////////////////////
 
-  test("JsonDecoder UserId") {}
+  test("JsonDecoder UserId") {
+    assert(userIdDecoder.decode("1234") == UserId(1234))
+    assert(userIdDecoder.decode("-1")  == UserId(-1))
+    Try(userIdDecoder.decode("hello")).isFailure
+  }
 
-  test("JsonDecoder LocalDate") {}
+  test("JsonDecode UserId PBT round-trip") {
+    forAll { (num: Int) =>
+      val json = num.toString
+      assert(userIdDecoder.decode(json) == UserId(num))
+    }
+  }
 
-  test("JsonDecoder weirdLocalDateDecoder") {}
+  test("JsonDecoder LocalDate") {
+    assert(localDateDecoder.decode("\"2020-03-26\"") == LocalDate.of(2020,3,26))
+    Try(localDateDecoder.decode("2020-03-26")).isFailure
+    Try(localDateDecoder.decode("hello")).isFailure
+  }
+
+  test("JsonDecoder round-trip") {
+    forAll(genLocalDate) { (localDate: LocalDate) =>
+      val dateString = DateTimeFormatter.ISO_LOCAL_DATE.format(localDate)
+      val json = s"\"$dateString\""
+      assert(localDateDecoder.decode(json) == localDate)
+    }
+  }
+
+  test("JsonDecoder weirdLocalDateDecoder") {
+    forAll(genLocalDate) { (localDate: LocalDate) =>
+      val dateJson = "\"" +  DateTimeFormatter.ISO_LOCAL_DATE.format(localDate) + "\""
+      val epochJson = localDate.toEpochDay.toString
+      assert(weirdLocalDateDecoder.decode(dateJson) == localDate)
+      assert(weirdLocalDateDecoder.decode(epochJson) == localDate)
+    }
+  }
+
+  test("optionDecoder ") {
+    assert(optionDecoder(stringDecoder).decode("null").isEmpty)
+    val date = LocalDate.of(2020,3,26)
+    assert(optionDecoder(weirdLocalDateDecoder).decode(date.toEpochDay.toString).get == date)
+  }
+
+  val genLocalDate: Gen[LocalDate] =
+    Gen
+      .choose(LocalDate.MIN.toEpochDay, LocalDate.MAX.toEpochDay)
+      .map(LocalDate.ofEpochDay)
+
+  implicit val arbitraryLocalDate: Arbitrary[LocalDate] =
+    Arbitrary(genLocalDate)
 
 }
